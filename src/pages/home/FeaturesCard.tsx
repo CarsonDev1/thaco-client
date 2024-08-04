5df4821c413c7e3a6c5d145ebfb6b91b3e37597b
CarsonDev1 <buitritinht@gmail.com>
2024-08-04 13:55:05 +0700

    update code

--- src/pages/home/FeaturesCard.tsx
+++ src/pages/home/FeaturesCard.tsx
@@ -15,16 +15,18 @@ const formatPrice = (price: number): string => {
 };
 
 const FeaturesCard: React.FC<IFeaturesCardProps> = ({ product }) => {
+	const { imageUrls = [], title = '', price = 0, discountPrice = 0 } = product || {};
+
 	return (
 		<div className='shadow-xl rounded-xl scale-95 hover:scale-100 transition-all duration-300 min-h-[300px] max-h-[300px] flex flex-col'>
 			<div className='bg-white p-4 flex flex-col gap-2 flex-grow'>
 				<div className='h-[150px] flex justify-center items-center'>
-					{product.imageUrls.length > 0 && (
+					{imageUrls.length > 0 && (
 						<Image
-							src={product.imageUrls[0]}
+							src={imageUrls[0]}
 							width={150}
 							height={150}
-							alt={product.title}
+							alt={title}
 							className='w-full h-full block object-cover'
 						/>
 					)}
@@ -34,12 +36,12 @@ const FeaturesCard: React.FC<IFeaturesCardProps> = ({ product }) => {
 						className='text-md block overflow-hidden'
 						style={{ display: '-webkit-box', WebkitLineClamp: 3, WebkitBoxOrient: 'vertical' }}
 					>
-						{product.title}
+						{title}
 					</span>
 				</div>
 				<div className='flex items-center gap-4'>
-					<p className='text-gray-600 mt-2 line-through'>{formatPrice(product.price)}</p>
-					<p className='text-rose-600 mt-2 font-semibold'>{formatPrice(product.discountPrice)}</p>
+					<p className='text-gray-600 mt-2 line-through'>{formatPrice(price)}</p>
+					<p className='text-rose-600 mt-2 font-semibold'>{formatPrice(discountPrice)}</p>
 				</div>
 			</div>
 		</div>
